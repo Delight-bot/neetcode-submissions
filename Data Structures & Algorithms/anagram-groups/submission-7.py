7c4420ea3fb18307e7baf9cfcbee74c02e2ccb7a
Delight Nyanhete <156990602+Delight-bot@users.noreply.github.com>
2026-09-18 21:58:47 -0500

    Add: anagram-groups - submission-7

---
+++ Data Structures & Algorithms/anagram-groups/submission-7.py
@@ -0,0 +1,32 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        '''
+        what do they have in common
+        they have the same letters back and forth
+        soln:
+        hash map were the sorted word is the key and the value 
+        is a list that appends the original word
+        edge cases:
+        if its empty return a list of empty and if its just 
+        one, return a a list of whatever is in there
+
+        eg: ["act","pots","tops","cat","tac","stop","hat"]
+        {"act":["act","cat","tac"],
+         "opst":[]
+        
+        }
+        return res = [[],[],[]]
+        '''
+        
+        anagrammap = defaultdict(list)
+
+        for i in strs: #o(n)
+            count = [0]*26 #
+            for char in i: #o(m)
+                count[ord(char)-ord('a')]+=1
+            anagrammap[tuple(count)].append(i)
+        #print(anagrammap.values())
+        return list(anagrammap.values())
+
+        
+        
